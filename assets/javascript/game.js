9c50084aea06053afae4b302b809e139846b8e47
Star Lee <zabra15@yahoo.com>
2017-08-25 17:10:50 -0400

    style changes and added functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,67 @@
 // Creates an array that lists out all of the possible movie titles.
 var movies = [
-    ["S", "P","E","E", "D"],
-    ["A", "L", "I", "E", "N"],
-    ["M", "A", "T", "R","I","X"]
+    "speed",
+    "alien",
+    "matrix",
+    "terminator",
+    "taken"
 ];
 
-// Creating variables to hold the number of guesses remaining and which letters have been guessed.
+// Create variables to hold the letters guessed and number of guesses remaining
 var lettersGuessed;
-
-
+var guessesLeft = 9;
 
 
     //Computer makes its guess
     // ?? -- HOW CAN I REPLACE THE LETTERS WITH BLANK SPACES UNTIL THEY'RE GUESSED?
     var computerChoice = movies[Math.floor(Math.random() * movies.length)];
-    document.getElementById("current-word").innerHTML = computerChoice;
+
+    var answerArray = [];
+
+        for (var i = 0; i < computerChoice.length; i++) {
+            answerArray[i] = "_";
+        }
+
+        var lettersLeft = computerChoice.length;
+
+//Display progress of game
+    document.getElementById("current-word").innerHTML = (answerArray.join(" "));
+
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
+        // Determines which key was pressed.
+        var userGuess = event.key;
+
+        for (var j = 0; j < computerChoice; j++) {
+            if (computerChoice[j] === userGuess) {
+                answerArray[j] = userGuess;
+                lettersLeft--;
+            } else {
+                guessesLeft--;
+            }
+        }
+        
 
 
     // Displays which guesses have been made
     document.getElementById("letters-guessed").innerHTML = userGuess;
 
-        // ?? -- HOW CAN I HOLD ON TO EACH GUESS INSTEAD OF OVERWRITING IT WITH EACH NEW GUESS?
-        var guessesLeft = 10;
-        var text = "";
-        var i;
-        for (i = 0; i < guessesLeft; i++) {
-            text += userGuess[i] + ", ";
-        }
+    // Displays guesses remaining
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+
+
+        // // ?? -- HOW CAN I HOLD ON TO EACH GUESS INSTEAD OF OVERWRITING IT WITH EACH NEW GUESS?
+        // var guessesLeft = 9;
+        // var text = "";
+        // var i;
+        // for (i = 0; i < guessesLeft; i++) {
+        //     text += userGuess[i] + ", ";
+        // }
 
     }
 
-document.getElementById("letters-guessed").onkeyup = function() {
-        var text = document.getElementById("")
-}
-
-
-
-
-    // ?? -- HOW CAN I REPLACE THE SPACES IN COMPUTERCHOICE WITH CORRECTLY GUESSED LETTERS?
 
 
 
@@ -53,36 +71,4 @@ document.getElementById("letters-guessed").onkeyup = function() {
 
 
 
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
 
-    // // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    // if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-    //
-    //     if ((userGuess === "r") && (computerChoice === "s")) {
-    //         wins++;
-    //     } else if ((userGuess === "r") && (computerChoice === "p")) {
-    //         losses++;
-    //     } else if ((userGuess === "s") && (computerChoice === "r")) {
-    //         losses++;
-    //     } else if ((userGuess === "s") && (computerChoice === "p")) {
-    //         wins++;
-    //     } else if ((userGuess === "p") && (computerChoice === "r")) {
-    //         wins++;
-    //     } else if ((userGuess === "p") && (computerChoice === "s")) {
-    //         losses++;
-    //     } else if (userGuess === computerChoice) {
-    //         ties++;
-    //     }
-
-        // // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        // var html =
-        //     "<p>You chose: " + userGuess + "</p>" +
-        //     "<p>The computer chose: " + computerChoice + "</p>" +
-        //     "<p>wins: " + wins + "</p>" +
-        //     "<p>losses: " + losses + "</p>" +
-        //     "<p>ties: " + ties + "</p>";
-        //
-        // // Set the inner HTML contents of the #game div to our html string
-        // document.querySelector("#game").innerHTML = html;
-//     }
-// };
